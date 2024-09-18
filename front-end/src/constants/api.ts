396597f246c32ffdf547f8a609e1ea4b962d511c
Robert Graupner <robert.graupnerr@gmail.com>
2024-09-18 22:26:07 +0200

    configure for vercel publish

--- front-end/src/constants/api.ts
+++ front-end/src/constants/api.ts
@@ -1,1 +1,3 @@
-export const BACK_END_URL = 'http://localhost:3000';
+export const BACK_END_URL = import.meta.env.PROD
+  ? 'https://audiophile-ecommerce-backend-i3g6.onrender.com'
+  : 'http://localhost:3000';
